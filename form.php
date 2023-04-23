db99833e452e87885c08968bb8affa6543ce8bf5
KoschavtcevDaniel <112947715+KoschavtcevDaniel@users.noreply.github.com>
2023-04-23 21:19:54 +0300

    Update form.php

--- form.php
+++ form.php
@@ -29,16 +29,21 @@ if (!empty($messages)) {
 <form action="index.php" method="POST">
   <div class = "main">
  <div class = "name"><label>FIO</label>
- <input name="fio" <?php if ($errors['fio']) {print 'class="error"';} ?> value="<?php print strip_tags($values['fio']); ?>" /></div>
+ <input name= "fio" <?php if ($errors['fio']) {print 'class="error"';} ?> value="<?php print strip_tags($values['fio']); ?>" /></div>
   <div class = "email"><label>E-MAIL</label>
   <input name="email" <?php if ($errors['email']) {print 'class="error"';} ?> value="<?php print strip_tags($values['email']); ?>" /></div>
   <div class = "year"><label>YEAR</label>
   <select name="year" <?php if ($errors['year']) {print 'class="error"';} ?> value="<?php print strip_tags($values['year']); ?>">
-    <?php 
-    for ($i = 1922; $i <= 2022; $i++) {
-      printf('<option value="%d">%d год</option>', $i, $i);
+  <?php
+       for($i=1920;$i<=2023;$i++){
+         if($values['year']==$i){
+          printf("<option value=%d selected>%d </option>",$i,$i);
+            }
+        else{
+          printf("<option value=%d>%d </option>",$i,$i);
+        }
     }
-    ?>
+   ?>
   </select></div>
   <div class = "sex" <?php if ($errors['sex']) {print 'class="error"';} ?>><label>Your SEX?</label>
   <label>Man</label><input type="radio" checked="checked" name="sex" value="0" <?php if($values['sex']=="0") {print 'checked';} ?>/>
@@ -54,8 +59,8 @@ if (!empty($messages)) {
   <input type="radio" name="limbs" value="16" <?php if($values['limbs']=="16") {print 'checked';} ?>/>
   </div>
 
-  <div class = "ability">  
-  <select name="abilities[]" multiple="multiple" <?php if ($errors['ability']) {print 'class="error"';} ?>> 
+  <div class = "ability" <?php if ($errors['abilities']) {print 'class="error"';} ?>>  
+  <select name="abilities[]" multiple="multiple"> 
     <option value="ab_in" <?php if($values['ab_in'] == 1){print 'selected';} ?>>невидимость</option>
     <option value="ab_t" <?php if($values['ab_t'] == 1){print 'selected';} ?>>телепортация</option>
     <option value="ab_l" <?php if($values['ab_l'] == 1){print 'selected';} ?>>левитация</option>
@@ -67,12 +72,42 @@ if (!empty($messages)) {
         <textarea name="biography" <?php if ($errors['biography']) {print 'class="error"';} ?>><?php print $values['biography']; ?></textarea>
   </div>
 
-  <div class="check" <?php if ($errors['check1']) {print 'error';} ?>>
-    <input type="checkbox" name="check1"  <?php if($values['check1']==TRUE){print 'checked';} ?>/> С контрактом ознакомлен(а)
-  </div>
-  <input type="submit" value="отправить" />
-  </div>
-</form>
+  <?php 
+      $lc='';
+       $ch='';
+     if($values['check1'] or !empty($_SESSION['login'])){
+      $ch='checked';
+      }
+     if ($errors['check1']) {
+      $lc='class="error"';
+      }
+      if(empty($_SESSION['login'])){
+       print('
+           <div  '.$lc.' >
+            <input name="check1" type="checkbox" '.$ch.'> Я согласен дать данные <br>
+            </div>');}
+     ?>
+
+                
+     <input type="submit" value="Отправить" />
+  </form>
+    <?php
+      if(empty($_SESSION['login'])){
+         echo'
+          <div class="login">
+             <p> <a href="login.php">Если имеется аккаунт, то нажмите здесь</a></p>
+          </div>';
+          }
+      else{
+          echo '
+            <div class="logout">
+              <form action="index.php" method="post">
+                  <input name="logout" type="submit" value="Выйти">
+              </form>
+            </div>';
+         } ?>
+
+       
   </div>
   </body>
 </html>
